641b65ac6a1813061f962d124fe8cccd18daa3d2
CurlyFire <stephane.denomme@inspq.qc.ca>
2018-12-19 13:57:06 -0500

    Added refreshing access tokens

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WebApp
 {
@@ -18,8 +24,46 @@ namespace WebApp
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
             })
-            .AddCookie()
-            .AddOpenIdConnect(options =>
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.Events = new CookieAuthenticationEvents()
+                {
+                    OnValidatePrincipal = async context =>
+                    {
+                        if (context.Principal.Identity.IsAuthenticated)
+                        {
+                            var accessToken = context.Properties.GetTokenValue(OpenIdConnectParameterNames.AccessToken);
+                            var refreshToken = context.Properties.GetTokenValue(OpenIdConnectParameterNames.RefreshToken);
+                            var jwtHandler = new JwtSecurityTokenHandler();
+                            var token = jwtHandler.ReadJwtToken(accessToken);
+                            var exp = DateTimeOffset.FromUnixTimeSeconds(token.Payload.Exp.Value).LocalDateTime;
+                            if (exp < DateTime.Now)
+                            {
+                                var httpClient = new HttpClient();
+                                var response = await httpClient.RequestRefreshTokenAsync(new RefreshTokenRequest()
+                                {
+                                    Address = "https://gia-dev3.santepublique.rtss.qc.ca/auth/realms/msss/protocol/openid-connect/token",
+                                    ClientId = "pqdcdev3",
+                                    ClientSecret = "a984903f-fdf2-464c-817d-596bbdd9fdbb",
+                                    RefreshToken = refreshToken
+                                });
+
+                                if (!response.IsError)
+                                {
+                                    context.Properties.UpdateTokenValue(OpenIdConnectParameterNames.RefreshToken, response.RefreshToken);
+                                    context.Properties.UpdateTokenValue(OpenIdConnectParameterNames.AccessToken, response.AccessToken);
+                                    context.ShouldRenew = true;
+                                }
+                                else
+                                {
+                                    context.RejectPrincipal();
+                                }
+                            }
+                        }
+                    }
+                };
+            })
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
             {
                 options.Authority = "https://gia-dev3.santepublique.rtss.qc.ca/auth/realms/msss";
                 options.ClientId = "pqdcdev3";
@@ -27,6 +71,7 @@ namespace WebApp
                 options.ResponseType = OpenIdConnectResponseType.Code;
                 options.SaveTokens = true;
                 options.GetClaimsFromUserInfoEndpoint = true;
+                options.UseTokenLifetime = true;
             });
         }
 
